362fa587c00594d45911cf16bc91f30f55fdad4b
SCDan0624 <dmayle1@hotmail.com>
2020-08-23 16:11:44 -0700

    added update user function

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -54,13 +54,25 @@ class UsersRepository {
     const filteredRecords = records.filter((record) => record.id !== id);
     await this.writeAll(filteredRecords);
   }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id of ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 // Error Test
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
-  await repo.delete("2667adc6");
+  await repo.update("a7d740fb", { password: "mypassword" });
 };
 
 test();
