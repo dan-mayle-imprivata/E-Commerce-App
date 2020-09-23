cf6b1da1f2d5a8a1232af39b74e58ad30563e0dd
SCDan0624 <dmayle1@hotmail.com>
2020-09-23 12:24:52 -0700

    HTML template function added to signin page

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
+const signinTemplate = require("../../views/admin/auth/signin");
 
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
-  res.send(res.send(signupTemplate({ req })));
-});
-
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersRepo.getOneBy({
-    email: email,
-  });
-  if (!user) {
-    return res.send("Email not found");
-  }
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send("Invalid password");
-  }
-
-  req.session.userId = user.id;
-
-  res.send("You are signed in!!!");
+  res.send(signupTemplate({ req }));
 });
 
 router.post("/signup", async (req, res) => {
@@ -57,15 +36,29 @@ router.get("/signout", (req, res) => {
 });
 
 router.get("/signin", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-      </form>
-    </div>
-    `);
+  res.send(signinTemplate());
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send("Email not found");
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+  if (!validPassword) {
+    return res.send("Invalid password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("You are signed in!!!");
 });
 
 module.exports = router;
