57d5e23e1930d211a01c91234b1696e4d8c13c2f
SCDan0624 <dmayle1@hotmail.com>
2020-10-15 18:12:30 -0700

    error messages added

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -4,9 +4,11 @@ const { check, validationResult } = require("express-validator");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
-const { requireEmail, requirePassword, requirePasswordConfirmation } = reqire(
-  "./validators"
-);
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation,
+} = require("./validators");
 
 const router = express.Router();
 
@@ -19,7 +21,10 @@ router.post(
   [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }));
+    }
 
     const { email, password, passwordConfirmation } = req.body;
     const user = await usersRepo.create({ email, password });
