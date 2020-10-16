89ee5716f0487fc0cca2595582debaf6f9418275
SCDan0624 <dmayle1@hotmail.com>
2020-10-16 16:56:12 -0700

    Sign in validation added

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -44,26 +44,46 @@ router.get("/signin", (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/signin",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must provide a valid email")
+      .custom(async (email) => {
+        const user = await usersRepo.getOneBy({ email });
+        if (!user) {
+          throw new Error("Email not found");
+        }
+      }),
+    check("password").trim().isLength({ min: 4, max: 20 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  const user = await usersRepo.getOneBy({ email });
+    const { email, password } = req.body;
 
-  if (!user) {
-    return res.send("Email not found");
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+      return res.send("Email not found");
+    }
+
+    const validPassword = await usersRepo.comparePasswords(
+      user.password,
+      password
+    );
+    if (!validPassword) {
+      return res.send("Invalid password");
+    }
+
+    req.session.userId = user.id;
+
+    res.send("You are signed in!!!");
   }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-  if (!validPassword) {
-    return res.send("Invalid password");
-  }
-
-  req.session.userId = user.id;
-
-  res.send("You are signed in!!!");
-});
+);
 
 module.exports = router;
