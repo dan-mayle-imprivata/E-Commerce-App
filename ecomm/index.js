8c384ff4de1d80a0ff97d140c7958ed580b89958
SCDan0624 <dmayle1@hotmail.com>
2020-10-17 16:43:38 -0700

    products routes started

--- ecomm/index.js
+++ ecomm/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
+const productsRouter = reqire("./routes/admin/products");
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(
 );
 
 app.use(authRouter);
+
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("Listening");
