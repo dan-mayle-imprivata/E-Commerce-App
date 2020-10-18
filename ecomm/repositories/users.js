b9cde8d433df55cb01b0463976a113117d2de9b3
SCDan0624 <dmayle1@hotmail.com>
2020-10-17 17:42:47 -0700

    Users repository refactored using classes

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -1,29 +1,19 @@
 const fs = require("fs");
 const crypto = require("crypto");
 const util = require("util");
+const Repository = require("./repository");
 
 const scrypt = util.promisify(crypto.scrypt);
 
-class UsersRepository {
-  constructor(filename) {
-    if (!filename) {
-      throw new Error("Creating a repository requires a filename");
-    }
+class UsersRepository extends Repository {
+  async comparePasswords(saved, supplied) {
+    // Saved -> password saved in database. 'hashed.salt'
+    // Supplied -> password given to us by user trying to sign in
 
-    this.filename = filename;
-    try {
-      fs.accessSync(this.filename);
-    } catch (err) {
-      fs.writeFileSync(this.filename, "[]");
-    }
-  }
+    const [hashed, salt] = saved.split(".");
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 
-  async getAll() {
-    return JSON.parse(
-      await fs.promises.readFile(this.filename, {
-        encoding: "utf8",
-      })
-    );
+    return hashed === hashedSuppliedBuf.toString("hex");
   }
 
   async create(attrs) {
@@ -43,68 +33,6 @@ class UsersRepository {
     await this.writeAll(records);
 
     return record;
-  }
-
-  async comparePasswords(saved, supplied) {
-    // Saved -> password saved in database. 'hashed.salt'
-    // Supplied -> password given to us by user trying to sign in
-
-    const [hashed, salt] = saved.split(".");
-    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
-
-    return hashed === hashedSuppliedBuf.toString("hex");
-  }
-
-  async writeAll(records) {
-    await fs.promises.writeFile(
-      this.filename,
-      JSON.stringify(records, null, 2)
-    );
-  }
-
-  randomId() {
-    return crypto.randomBytes(4).toString("hex");
-  }
-
-  async getOne(id) {
-    const records = await this.getAll();
-    return records.find((record) => record.id === id);
-  }
-
-  async delete(id) {
-    const records = await this.getAll();
-    const filteredRecords = records.filter((record) => record.id !== id);
-    await this.writeAll(filteredRecords);
-  }
-
-  async update(id, attrs) {
-    const records = await this.getAll();
-    const record = records.find((record) => record.id === id);
-
-    if (!record) {
-      throw new Error(`Record with id ${id} not found`);
-    }
-
-    Object.assign(record, attrs);
-    await this.writeAll(records);
-  }
-
-  async getOneBy(filters) {
-    const records = await this.getAll();
-
-    for (let record of records) {
-      let found = true;
-
-      for (let key in filters) {
-        if (record[key] !== filters[key]) {
-          found = false;
-        }
-      }
-
-      if (found) {
-        return record;
-      }
-    }
   }
 }
 
