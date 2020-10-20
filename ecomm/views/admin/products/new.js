ab322bd70818266fffce919ab61cf6a14299ab9f
SCDan0624 <dmayle1@hotmail.com>
2020-10-20 15:32:54 -0700

    Error bug resolved

--- ecomm/views/admin/products/new.js
+++ ecomm/views/admin/products/new.js
@@ -6,7 +6,9 @@ module.exports = ({ errors }) => {
     content: `
         <form method="POST" enctype="multipart/form-data">
             <input placeholder="Title" name="title" />
+            ${getError(error, "title")}
             <input placeholder="Price" name="price" />
+            ${getError(error, "price")}
             <input type="file" name="image" />
             <button>Submit</button>
         </form>
