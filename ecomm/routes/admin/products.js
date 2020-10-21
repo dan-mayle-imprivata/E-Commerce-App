4c6a8f008d1ad571748f997a383c532cc818a606
SCDan0624 <dmayle1@hotmail.com>
2020-10-21 12:44:54 -0700

    error handling middleware added

--- ecomm/routes/admin/products.js
+++ ecomm/routes/admin/products.js
@@ -1,39 +1,31 @@
-const express = require("express");
-const { validationResult } = require("express-validator");
-const multer = require("multer");
+const express = require('express');
+const multer = require('multer');
 
-const productsRepo = require("../../repositories/products");
-const productsNewTemplate = require("../../views/admin/products/new");
-const { requireTitle, requirePrice } = require("./validators");
+const { handleErrors } = require('./middlewares');
+const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", (req, res) => {});
+router.get('/admin/products', (req, res) => {});
 
-router.get("/admin/products/new", (req, res) => {
+router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
-  "/admin/products/new",
-  upload.single("image"),
+  '/admin/products/new',
+  upload.single('image'),
   [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(productsNewTemplate({ errors }));
-    }
-    const image = req.file.buffer.toString("base64");
+    const image = req.file.buffer.toString('base64');
     const { title, price } = req.body;
-    await productsRepo.create({
-      title,
-      price,
-      image,
-    });
+    await productsRepo.create({ title, price, image });
 
-    res.send("submitted");
+    res.send('submitted');
   }
 );
 
--- ecomm/views/helpers.js
+++ ecomm/views/helpers.js
@@ -1,9 +1,9 @@
 module.exports = {
   getError(errors, prop) {
     try {
-      return error.mapped()[prop].msg;
+      return errors.mapped()[prop].msg;
     } catch (err) {
-      return "";
+      return '';
     }
-  },
+  }
 };
